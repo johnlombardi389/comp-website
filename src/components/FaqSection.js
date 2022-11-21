f9d189654c12f7b11d9ef7d6add05cce0655cc38
johnlombardi389 <lombardij38@gmail.com>
2022-11-21 15:50:22 -0500

    Created FAQ component for about page

---
+++ src/components/FaqSection.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I do something?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            nesciunt.
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>How do I do something?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            nesciunt.
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>How do I do something?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            nesciunt.
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>How do I do something?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            nesciunt.
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
