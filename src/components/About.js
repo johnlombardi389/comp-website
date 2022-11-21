a8ed03a9dd2fb5a130f289de67c212b1053326a3
johnlombardi389 <lombardij38@gmail.com>
2022-11-21 14:45:01 -0500

    Added img for about section

---
+++ src/components/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+import travelLuggage from "../img/travelLuggage.jpg";
+
+function About() {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>Title</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              Another title <span>for this website</span>
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>Title 3</h2>
+          </div>
+          <p>Contact us for any help traveling</p>
+          <button>Contact Us</button>
+        </div>
+        <div className="image">
+          <img
+            src={travelLuggage}
+            alt="Waiting in airport with luggage watching plane"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
