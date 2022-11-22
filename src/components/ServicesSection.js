482ac5fc3d26dde21633dd28262d721ead8761b7
johnlombardi389 <lombardij38@gmail.com>
2022-11-21 22:09:35 -0500

    Styled services section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -3,53 +3,86 @@ import React from "react";
 import { FaPlane, FaClock, FaMoneyBillAlt, FaGlobe } from "react-icons/fa";
 // Image
 import mapHand from "../img/mapHand.jpg";
+// Styles
+import styled from "styled-components";
+import { About, Description, Image } from "../styles";
 
 function ServicesSection() {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           Our <span>services include many things</span>
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <FaPlane />
               <h3>Find flight</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <FaClock />
               <h3>24 hour support</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <FaMoneyBillAlt />
               <h3>Money exchange</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <FaGlobe />
               <h3>Discover destinations</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <div className="image">
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={mapHand} alt="Planning trip with map" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 }
+
+// Styles
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
