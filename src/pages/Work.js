ecfbe2276f8047b06babe2b5e4c28b1598d7b6d4
johnlombardi389 <lombardij38@gmail.com>
2022-11-22 14:01:13 -0500

    Began workin on work page and added basic styling

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// Styles
+import styled from "styled-components";
+// Images
 
 function Work() {
   return (
-    <div>
-      <h1>Work</h1>
-    </div>
+    <StyledWork>
+      <Items>
+        <h2>Item Name</h2>
+        <div className="line"></div>
+        <Link>
+          <img src="" alt="" />
+        </Link>
+      </Items>
+    </StyledWork>
   );
 }
+
+const StyledWork = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Items = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default Work;
