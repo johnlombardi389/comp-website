bc8dff3524c7263837a1ab6b609a6f24aa755734
johnlombardi389 <lombardij38@gmail.com>
2022-11-21 21:44:05 -0500

    Created styles.js to hold styled components that will be reused for different sections

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 import travelLuggage from "../img/travelLuggage.jpg";
+// Styles
+import { About, Description, Image, Hide } from "../styles";
 
 function AboutSection() {
   return (
@@ -31,37 +32,5 @@ function AboutSection() {
     </About>
   );
 }
-
-// Styled components
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
