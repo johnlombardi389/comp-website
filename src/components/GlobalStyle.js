98c8d4eb5ac4258c217f1b05ccfd0f0e967dd19b
johnlombardi389 <lombardij38@gmail.com>
2022-11-21 20:04:46 -0500

    Styled global styles of page for font colors and background and buttons

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -25,6 +25,35 @@ const GlobalStyle = createGlobalStyle`
       color: white;
     }
   }
+
+  h2 {
+      font-weight: lighter;
+      font-size: 4rem;
+    }
+
+  h3 {
+      color: white;
+    }
+
+  h4 {
+    font-weight: bold;
+  }
+
+  a {
+    font-size: 1.1rem;
+  }
+
+  span {
+    font-weight: bold;
+    color: #23d997;
+  }
+
+  p {
+      padding: 3rem 0rem;
+      color: #ccc;
+      font-size: 1.4rem;
+      line-height: 150%;
+    }
 `;
 
 export default GlobalStyle;
