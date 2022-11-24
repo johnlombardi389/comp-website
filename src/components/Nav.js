061cbe20635ba5bf8ba7d38ea072841293d645b0
johnlombardi389 <lombardij38@gmail.com>
2022-11-24 14:06:26 -0500

    Changed nav link positions for smaller screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -54,6 +54,22 @@ const StyledNav = styled.nav`
     padding-left: 10rem;
     position: relative;
   }
+  @media (max-width: 1300px) {
+    flex-direction: column;
+    padding: 1rem;
+    #logo {
+      display: inline-block;
+      margin-bottom: 2rem;
+    }
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+      li {
+        padding: 0;
+      }
+    }
+  }
 `;
 
 export default Nav;
